a77bd818d7b12201efb44f460482857fd9216d98
imajoriri <imajo.srroni@gmail.com>
2019-01-26 10:15:06 +0900

    Updates

---
+++ precache-manifest.d3725d8df7c80410342c7882ce11e903.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d6a3676356467f0ac3a",
+    "url": "/liff-izvote/static/js/runtime~main.8d6a3676.js"
+  },
+  {
+    "revision": "19f618b5ed8d1db194df",
+    "url": "/liff-izvote/static/js/main.19f618b5.chunk.js"
+  },
+  {
+    "revision": "93cd9b117c214ab609b3",
+    "url": "/liff-izvote/static/js/1.93cd9b11.chunk.js"
+  },
+  {
+    "revision": "19f618b5ed8d1db194df",
+    "url": "/liff-izvote/static/css/main.a62efa23.chunk.css"
+  },
+  {
+    "revision": "b1aa647c9a91514a7f15e78a9e76efd4",
+    "url": "/liff-izvote/index.html"
+  }
+];

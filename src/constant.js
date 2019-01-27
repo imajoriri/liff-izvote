2c8be4488737c72726073c79c0088313ab691c0b
imajoriri <imajo.srroni@gmail.com>
2019-01-27 17:13:58 +0900

    update

---
+++ src/constant.js
@@ -0,0 +1,12 @@
+export default {
+  lambdaIzvoteCreatePlan: "https://itpqrsv52e.execute-api.ap-northeast-1.amazonaws.com/prod/izvote-create-plan",
+  firebaseConfig: {
+    apiKey: "AIzaSyAl3vOW03SJL9wYjZmIa9L9QNDxp0lCF-0",
+    authDomain: "izvote.firebaseapp.com",
+    databaseURL: "https://izvote.firebaseio.com",
+    projectId: "izvote",
+    storageBucket: "izvote.appspot.com",
+    messagingSenderId: "180735679178"
+  },
+}
+
